ab236dd9095a2f418201cfb881bb2e8e85977986
karthickyuvan <68184584+karthickyuvan@users.noreply.github.com>
2024-11-16 19:19:31 +0530

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,60 @@
+ exercise - 1 
+
+/*
+var rain = true
+if(rain)
+{
+    console.log("take Umbrella")
+}
+else
+{
+    console.log("Enjoy the Sunshine")
+}
+*/
+-------------------------------------------------
+ exercise - 2
+/*
+var scrore = 51
+
+if(scrore <= 50 )
+{
+    console.log("You Need to Improve")
+}
+else if (scrore > 50 && scrore <= 70)
+{
+    console.log("Good Job")
+}
+else if(scrore > 70 )
+{
+    console.log("Excellent")
+}
+*/
+-------------------------------------------------
+exercise - 3
+/*
+var input = 21
+if(input % 2  == 0)
+{
+    console.log(" the give number is Even " + " the Number is " + input)
+}
+else
+{
+    console.log(" The given Number is odd and the no is " + input)
+}
+*/
+-------------------------------------------------
+ exercise - 4
+
+var vowels= 'a'
+
+vowels = vowels.toLowerCase()
+
+    if (vowels ==='a' || vowels === 'e' || vowels ==='i' ||vowels === 'o' || vowels ==='u' )
+    {
+        console.log(" the Given Input is vowels and the given input was " + vowels)
+    }
+    else{
+        console.log(" the Given Input is not a vowels and the given input was " + vowels)
+    }
+
+-------------------------------------------------
