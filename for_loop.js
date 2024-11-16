38fd57297ff8ac2f32d1c8b80a3f4f4e08fa574e
karthickyuvan <68184584+karthickyuvan@users.noreply.github.com>
2024-11-16 19:20:23 +0530

    Create for_loop.js

---
+++ for_loop.js
@@ -0,0 +1,60 @@
+// practise - 1
+
+/*
+ for( count = 0 ; count <=10 ; count=count+1)
+ {
+    console.log(count + " JOhn")
+ }
+ */
+
+
+ //practise - 2 
+
+ /*
+ for (count = 1 ;count <=10 ; count = count +2)
+ {
+    console.log(count)
+ }
+ */
+
+  //practise - 3 print in reverse order 10-1
+
+  /*
+  for(count = 10 ; count >=1 ; count = count -1)
+  {
+    console.log(count)
+
+  }
+ */
+
+
+   //practise - 4 - print Even Numbers
+
+
+  /*
+ for( count = 1 ; count <=10 ; count = count +1)
+ {
+    if( count % 2===0)
+    {
+        console.log(count)
+    }
+ }
+ */ 
+
+ //practise - 5 - print 2 tables
+ 
+// var number = 2
+/*
+ for (int= 1 ; int <=10;int=int +1)
+ {
+    
+        console.log(int + "*2=" + (int * 2) )
+   
+   
+ }
+
+ */
+
+
+
+
